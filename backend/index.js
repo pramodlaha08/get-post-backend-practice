75be7e9b4f45ab3ff966cece527a3773f5e4c1fe
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-12-26 21:11:30 +0545

    Enhance request handling in backend: add URL-encoded body parsing and improve response messages

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({extended: true}))
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 })
 
 app.get('/request', (req, res) => {
-    res.send('send request with get request');
+    let { name, password } = req.query;
+    res.send(`send request with get request. Welcome ${name} with password ${password}`);
+
 })
 app.post('/request', (req, res) => {
-    res.send('send request with post request');
+    res.send('send request with post request. Welcome ', name);
+    let { name, password } = req.body;
+    console.log(`Welcome ${name} with password ${password}`);
 })
